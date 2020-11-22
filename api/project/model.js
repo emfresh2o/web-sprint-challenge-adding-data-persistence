e3bf49e2e7e1b45ad589ebcc1898ce73de580df9
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2020-11-22 10:16:00 -0500

    working on routers ... error knex migration...

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,33 @@
 // build your `Project` model here
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+    getProjects,
+    addProjects,
+    findById, 
+    remove
+}
+
+function getProjects(){
+    return db('project')
+}
+
+function findById(id){
+    return db('project')
+        .where({id})
+        .first();
+}
+
+function addProjects(project){
+    return db('project')
+        .insert(project, 'id')
+        .then(id => {
+            return findById(id[0])
+        })
+}
+
+function remove(id) {
+    return db('project')
+    .where({ id })
+    .del();
+}
